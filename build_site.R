0cd3f232e85824c0d721325de9418050b847788e
npg2108 <npg2108@cumc.columbia.edu>
2017-10-01 22:03:35 -0400

    Added tuturial links

--- build_site.R
+++ build_site.R
@@ -3,12 +3,18 @@
 #not our site because of other projects we were 
 #working on at the time. 
 
+#Tutorials referenced:
+#http://rmarkdown.rstudio.com/rmarkdown_websites.html
+#http://nickstrayer.me/RMarkdown_Sites_tutorial/
+#https://jules32.github.io/rmarkdown-website-tutorial/index.html
+#http://livefreeordichotomize.com/2017/08/08/how-to-make-an-rmarkdown-website/  
+
 #Make sure the wd is in ngiangre.github.io/! 
 #(Open with RProject)
 
-#convert markdiwn CV
+#convert markdown CV
 system("./CV/build_CV.sh")
 
-#render your sweet site. 
+#render site. 
 rmarkdown::render_site()
 
